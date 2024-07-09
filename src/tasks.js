e58cbfc840383f81d8a161b390093384893de49d
robin-england <robin.england94@googlemail.com>
2024-07-09 17:25:03 -0400

    Set tasks array in tasks.js

---
+++ src/tasks.js
@@ -0,0 +1,7 @@
+export const tasks = [
+    { id: 0, task: 'Clean Dishes', isCompleted: true },
+    { id: 1, task: 'Complete Schoolwork', isCompleted: false },
+    { id: 2, task: 'Call Parents', isCompleted: false },
+    { id: 3, task: 'Wash Car', isCompleted: true },
+    { id: 4, task: 'Take out the trash', isCompleted: false }
+]
